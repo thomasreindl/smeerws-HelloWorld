cb899586f6091cad845df7bfed5769366767cf27
unknown <hello@xrux.at>
2018-11-01 12:27:02 +0100

    mod rename

---
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿//smeerws: 
+//printing messages to the unity console
+
+using System;
+using UnityEngine;
+
+
+public class HelloWorld : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        string hw = "Hello World (hw)";
+
+        print("(print) Hello World");
+        Debug.Log("(Debug.Log) Hello World");
+        Console.WriteLine("(Console.WriteLine) Hello World"); //Console.WriteLine does nothing inside the unity Engine,
+                                                              //because Unity uses a custom console, only accessed 
+                                                              //through Unity engine: Debug.Log, ...
+                                                              //Unity doesn't display the standard C# Console
+
+        print("(print - hw) " + hw);
+        Debug.Log("(Debug.Log - hw ) " + hw);
+        Console.WriteLine("Console.WriteLine - hw" + hw);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # smeerws-HelloWorld
+
+Development Software: Unity version: 2018.2.8f1,  Visual Studio 2017 (v 15.4.5)
+Hello World, first C# script answering the question: **How do I write to the console.**  
