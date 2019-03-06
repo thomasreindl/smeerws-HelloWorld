72c547344166a3af182d2ec70e68d812e32b9258
thomasreindl <reindlthomas@gmx.at>
2019-03-06 17:11:40 +0100

    review code

--- Assets/MyGame/Scripts/HelloWorld.cs
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -1,31 +1,23 @@
 ﻿//smeerws: 
 //printing messages to the unity console
-
 using System;
 using UnityEngine;
 
-
-public class HelloWorld : MonoBehaviour {
-
-
-	// Use this for initialization
-	void Start () {
-        string hw = "Hello World (hw)";
+public class HelloWorld : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        string helloWorldText = "Hello World (hw)";
 
         print("(print) Hello World");
         Debug.Log("(Debug.Log) Hello World");
-        Console.WriteLine("(Console.WriteLine) Hello World"); //Console.WriteLine does nothing inside the Unity Engine,
-                                                              //because Unity uses a custom console, only accessed 
-                                                              //through Unity Engine: Debug.Log, ...
-                                                              //Unity doesn't display the standard C# Console.
-
-        print("(print - hw) " + hw);
-        Debug.Log("(Debug.Log - hw ) " + hw);
-        Console.WriteLine("Console.WriteLine - hw" + hw);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Console.WriteLine("(Console.WriteLine) Hello World");   //Console.WriteLine does nothing inside the Unity Engine,
+                                                                //because Unity uses a custom console, only accessed 
+                                                                //through Unity Engine: Debug.Log, ...
+                                                                //Unity doesn't display the standard C# Console
+        print("(print - helloWorldText)" + helloWorldText);
+        Debug.Log("(Debug.Log - helloWorldText)" + helloWorldText);
+        Console.WriteLine("Console.WriteLine - helloWorldText)" + helloWorldText);
+    }
 }
